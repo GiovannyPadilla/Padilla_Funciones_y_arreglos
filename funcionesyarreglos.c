6e3eb2939c2d6fa6cf1531acb07a5a2c99334a2f
Giovanny Padilla <giovanny.padilla@udla.edu.ec>
2023-12-05 20:39:59 -0500

    Función para ingresar las dimensiones de la matriz

---
+++ funcionesyarreglos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Función para ingresar las dimensiones de la matriz
+void DimensionesMatriz(int *filas, int *columnas) {
+    printf("Ingrese el número de filas:\n");
+    scanf("%d", filas);
+    printf("Ingrese el número de columnas:\n");
+    scanf("%d", columnas);
+}
