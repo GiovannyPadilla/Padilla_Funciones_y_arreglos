837408c293cdbcc7709c5bff25749f7f1195c521
Giovanny Padilla <giovanny.padilla@udla.edu.ec>
2023-12-05 20:44:10 -0500

    generar m,atriz aleatoria

--- funcionesyarreglos.c
+++ funcionesyarreglos.c
@@ -9,3 +9,18 @@ void DimensionesMatriz(int *filas, int *columnas) {
     printf("Ingrese el número de columnas:\n");
     scanf("%d", columnas);
 }
+//generar matriz aleatoria
+
+void MatrizAleatoria(int filas, int columnas, int matriz[filas][columnas]) {
+    srand(time(NULL));
+
+    printf("\nMatriz:\n");
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = rand() % 100; // Generar un número aleatorio entre 0 y 99
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
