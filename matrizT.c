8d931ab2f8de7aa2abc870ddc7414061e00f6e6e
Giovanny Padilla <giovanny.padilla@udla.edu.ec>
2023-12-05 21:01:50 -0500

    inicializar ultima matriz en unos

--- matrizT.c
+++ matrizT.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 // Función para inicializar una matriz en ceros
-void inicializarMatrizCeros(int filas, int columnas, int limite, int matriz[columnas][filas][limite]) {
+void MatrizCeros(int filas, int columnas, int limite, int matriz[columnas][filas][limite]) {
     for (int n = 0; n < columnas; n++) {
         for (int m = 0; m < filas; m++) {
             for (int p = 0; p < limite; p++) {
                 matriz[n][m][p] = 0;
             }
+        }
+    }
+}
+
+// Función para inicializar la última matriz en unos
+void MatrizUnos(int filas, int columnas, int limite, int matriz[columnas][filas][limite]) {
+    for (int m = 0; m < filas; m++) {
+        for (int p = 0; p < limite; p++) {
+            matriz[columnas - 1][m][p] = 1;
         }
     }
 }
